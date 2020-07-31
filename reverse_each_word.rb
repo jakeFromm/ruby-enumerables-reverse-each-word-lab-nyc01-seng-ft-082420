23ad6ef402ff1e126908145005d066ec8fbd07ed
Jacob Fromm <jacobfromm@gmail.com>
2020-07-31 20:28:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 def reverse_each_word(string)
   arr = string.split
   new_arr = []
-  arr.each {|el| el.reverse}
+  arr.each {|el| arr[el].reverse}
 end  
 
 #puts reverse_each_word("hello world")
